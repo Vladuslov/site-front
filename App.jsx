fc13cfa2801982cbec4fec7c249fa3986b3977ed
Vladislav <76568831+Vladuslov@users.noreply.github.com>
2022-10-30 21:58:07 +0400

    front-site

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+import Modal from './ModaL/Modal';
+
+const App = () => {
+    const [modaAction, setModalAction] = useState(true);
+    return (
+        <div className="app">
+            <main></main>
+            </div>
+    );
+};
---
+++ ModaL/Modal.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./style.css"
+
+const Modal = ({active, setActive}) => {
+    return (
+        <div className="modal">
+            <div className="modal__content">
+
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
